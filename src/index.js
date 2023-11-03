7129a7c739adedd5c66b93090f70b750f284dc9c
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-03 07:57:22 +0000

    Establish basic grid layout.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ const loadPage = (function () {
     const headerContent = manipulateDOM.createContainer ('header');
     const sidebarContent = manipulateDOM.createContainer ('sidebar');
     const displayContent = manipulateDOM.createContainer ('display');
+    const gridContainer = manipulateDOM.createContainer ('grid');
 
     manipulateDOM.addToHTML (headerContent, htmlContent);
-    manipulateDOM.addToHTML (sidebarContent, htmlContent);
-    manipulateDOM.addToHTML (displayContent, htmlContent);
+    manipulateDOM.addToHTML (gridContainer, htmlContent);
+    manipulateDOM.addToHTML (sidebarContent, gridContainer);
+    manipulateDOM.addToHTML (displayContent, gridContainer);
 
     manipulateDOM.addToHTML (manipulateDOM.createTextElement ('h2', 'To Do List'), headerContent);
-    manipulateDOM.addToHTML (manipulateDOM.createTextElement ('button', 'Add Project'), headerContent)
+    manipulateDOM.addToHTML (manipulateDOM.createTextElement ('button', 'Add Project'), headerContent);
+
+    manipulateDOM.addToHTML (manipulateDOM.createTextElement ('span', 'Projects'), sidebarContent);
+
+    manipulateDOM.addToHTML (manipulateDOM.createTextElement ('div', 'Today'), displayContent)
 
 })();
 
