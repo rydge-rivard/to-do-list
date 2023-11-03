32c0a4cc2b69f75bb07338645a17b12591d87ede
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-03 12:21:52 +0000

    Bind click event to confirm button in dialog.

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,21 @@
 export {modalMod}
+import {useDOM} from "./dom";
 
 const modalMod = (function () {
 
     const dialog = document.querySelector('dialog')
+    const confirmBtn = dialog.querySelector("#confirmBtn");
+
+    useDOM.bindEvents (confirmBtn, 'click', (event) => confirmTask (event));
 
     function showModal () {
         console.log('click')
         dialog.showModal();
+    }
+
+    function confirmTask (event) {
+        event.preventDefault();
+        console.log ('create task')
     }
 
     return {
