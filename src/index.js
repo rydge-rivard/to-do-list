6d5d3cecf6da5bb047bac4540a1fbb9062935778
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-07 15:32:00 +0100

    Update default data for show and tell.

--- src/index.js
+++ src/index.js
@@ -35,18 +35,30 @@ const loadPage = (function () {
 
     useDOM.addToHTML (useDOM.createTextElement ('h3', 'Projects'), sidebarContent);
 
-    const todayTasks = [];
-    const clean = taskMod.createTask ('Clean', 'clean your room', '2023-12-31', 'Low', `Clean-id#0`);
-    taskMod.assignTask (clean, todayTasks);
-    const surf = taskMod.createTask ('Surf', 'high tide at 3PM', '2023-12-31', 'High', `Surf-id#1`);
-    taskMod.assignTask (surf, todayTasks);
+    const mealTasks = [];
+    const mon = taskMod.createTask ('Pasta', 'pick up pesto and noodles for Monday',
+     '2023-12-29', 'Low', `Pasta-id#0`);
+    taskMod.assignTask (mon, mealTasks);
+    const tues = taskMod.createTask ('Fish', 'head to the market and pick up something fresh',
+     '2023-12-30', 'High', `Fish-id#1`);
+    taskMod.assignTask (tues, mealTasks);
+    const wed = taskMod.createTask ('Burgers', 'Wednesday is supposed to be great BBQ weather',
+     '2023-12-31', 'Medium', `Burgers-id#2`);
+    taskMod.assignTask (wed, mealTasks);
 
     const moroccoTasks = [];
-    const cook = taskMod.createTask ('Cook', 'tagine', '2023-12-31', 'Medium', `Cook-id#0`);
+    const cook = taskMod.createTask ('Cook', 'try to cook tagine like the locals',
+     '2023-12-1', 'Medium', `Cook-id#0`);
     taskMod.assignTask (cook, moroccoTasks);
+    const shop = taskMod.createTask ('Shop', 'go shopping for a cool new rug to bring home',
+     '2023-12-3', 'None', `Shop-id#1`);
+    taskMod.assignTask (shop, moroccoTasks);
+    const camel = taskMod.createTask ('Feed a Camel', 'find out what camels eat and feed one',
+    '2023-12-5', 'Medium', `Feed-id#2`);
+   taskMod.assignTask (camel, moroccoTasks);
 
     projectMod.projects.push (projectMod.createProject ('Morocco', moroccoTasks));
-    projectMod.projects.push (projectMod.createProject ('Today', todayTasks));
+    projectMod.projects.push (projectMod.createProject ('Meal Plan', mealTasks));
 
     useDOM.renderDisplay (projectMod.projects[0], gridContainer);
     useDOM.renderSidebar (projectMod.projects, sidebarContent, 'div', gridContainer)
